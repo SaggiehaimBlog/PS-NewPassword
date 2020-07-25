8c30ad1212c394f7d2920906792b244ceef70ec3
saggie haim <saggiehaim@gmail.com>
2020-07-25 15:36:03 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# PS-NewPassword
-PowerShell module to generate password for automations process or any other needs. 
+# New Password Module Version 2.0.0.
+
+Generate password for any automation process or any other needs.
+you can download it from the powershell gallery: https://www.powershellgallery.com/packages/NewPassword/2.0.0
+All feedback welcomed!
+
+contact me at contact@saggiehaim.net
+
+https://www.saggiehaim.net/
+
+
+## DESCRIPTION 
+The New-Password cmdlet allow you to generate random passwords. 
+   
+### PARAMETER NumberofPasswords 
+
+    Specifies Number of passwored to generate. Default is 1 random password.
+
+### PARAMETER AsSecureString
+     Return passwords as secure strings.
+ 
+
+### PARAMETER Length 
+     Specifies the passwored length. Defualt is 12 chars.
+ 
+### PARAMETER SpecialCharacters
+     Specifies the number of Special Characters in the password. Defualt is 3 chars.
+  
+## EXAMPLES
+### Generate Random Password
+```PowerShell 
+New-Password 
+S0XOxe8SvaYW
+```
+### Generate 10 Random Password
+```PowerShell
+New-Password -NumberofPasswords 10
+VMLMu2DVLg6a
+H0faw#W6N@@H
+WZ0FV7U7jbVB
+p6vDelWGmefS
+HR9Ewjg#WQt3
+qrHYJdRTWaex
+q6meEJztl80R
+oNDals6@DrI7
+kKJVCbvAhFXm
+f4@8xTsUYpAI
+```
+### Generate 1 Passowrd with 20 chars length
+```PowerShell
+New-Password -Length 20
+e00yosTnRvthCHiJYKJs
+```
+### Generate 5 Passwords that are 6 chars length
+```PowerShell
+New-Password -NumberofPasswords 5 -Length 6
+ZSQHsL
+RqU5cY
+F7STnb
+DD34@O
+9drhe4
+```
+### Generate password as a secure string (good for automation scripts)
+```PowerShell
+New-Password -NumberofPasswords 1 -Leng10 -AsSecureString
+WARNING: Converting the Passwo3@jedeVcjoBf to secure string, this is tlast time you will see it.
+System.Security.SecureString
+```
+### Generate password and specify number of the special characters in the password
+```PowerShell
+New-Password -NumberofPasswords 5 -Length 10 -SpecialCharacters 3
+#zx4?1R@p1
+>xv=2RaAdG
+<u<4TDJAzL
+```
---
+++ src/NewPassword.psm1
@@ -0,0 +1,129 @@
+﻿## Generate Password Function
+Function New-Password() {
+    <# 
+ .SYNOPSIS 
+     Password Generator 
+ 
+ .DESCRIPTION 
+     The New-Password cmdlet allow you to generate random passwords. 
+   
+ .PARAMETER NumberofPasswords 
+     Specifies Number of passwored to generate. Default is 1 random password. 
+
+ .PARAMETER AsSecureString
+     Return passwords as secure strings.
+ 
+ .PARAMETER Length 
+     Specifies the passwored length. Defualt is 12 chars.
+ 
+ .PARAMETER SpecialCharacters
+     Specifies the number of Special Characters in the password. Defualt is 3 chars.
+  
+ .EXAMPLE 
+        New-Password 
+        S0XOxe8SvaYW
+
+  .EXAMPLE
+        New-Password -NumberofPasswords 10
+        VMLMu2DVLg6a
+        H0faw#W6N@@H
+        WZ0FV7U7jbVB
+        p6vDelWGmefS
+        HR9Ewjg#WQt3
+        qrHYJdRTWaex
+        q6meEJztl80R
+        oNDals6@DrI7
+        kKJVCbvAhFXm
+        f4@8xTsUYpAI
+
+    .EXAMPLE
+            New-Password -Length 20
+            e00yosTnRvthCHiJYKJs
+
+    .EXAMPLE
+            New-Password -NumberofPasswords 5 -Length 6
+            ZSQHsL
+            RqU5cY
+            F7STnb
+            DD34@O
+            9drhe4
+    
+    .EXAMPLE
+            New-Password -NumberofPasswords 1 -Length 10 -AsSecureString
+            WARNING: Converting the Password 3@jedeVcjoBf to secure string, this is the last time you will see it.
+            System.Security.SecureString
+    
+    .EXAMPLE
+            New-Password -NumberofPasswords 5 -Length 10 -SpecialCharacters 3
+            #zx4?1R@p1
+            >xv=2RaAdG
+            <u<4TDJAzL
+
+
+ .NOTES 
+  Author: Saggie Haim 
+  Contact@saggiehaim.net
+ #>
+    [cmdletbinding()]
+    param (
+        [parameter(Position = 0,
+            Mandatory = $false)]
+        [string]$NumberofPasswords = 1,
+
+        [parameter(Position = 1,
+            Mandatory = $false)]
+        [string]$Length = 12,
+
+        [Parameter(Position = 2,
+            Mandatory = $false)]
+        [switch]$AsSecureString = $false,
+
+        [parameter(Position = 3,
+            Mandatory = $false)]
+        [string]$SpecialCharacters = 3
+    )
+    $passwords = @()
+    $inputRange = 48..122
+    $SpecialCharactersRange = 33, 35, 60, 61, 62, 63, 64
+    $exclude = 91..96
+    $exclude += 58..64
+    $randomRange = $inputRange | Where-Object { $exclude -notcontains $_ }
+    For ($j = 0; $j -lt $NumberofPasswords; $j++) {
+        $pass = $null
+        For ($i = 0; $i -lt ($Length - $SpecialCharacters); $i++) {
+            $rnd = (Get-Random -InputObject $randomRange) 
+            $char = [char]$rnd
+            $pass += $char
+        }
+        for ($k = 0; $k -lt $SpecialCharacters; $k++) {
+            $rnd = (Get-Random -InputObject $SpecialCharactersRange) 
+            $char = [char]$rnd
+            $pass += $char
+        }
+        $pass = RandomisePassword $pass
+        $passwords += $pass
+        
+    }
+    if ($AsSecureString -eq $true) {
+        $passwords = $passwords | ForEach-Object { Write-Warning "Converting the Password $_ to secure string, this is the last time you will see it."; ConvertTo-SecureString -String $_ -AsPlainText -Force }
+        return $passwords
+    }
+    Return $passwords
+}
+
+function RandomisePassword {
+    [cmdletbinding()]
+    param (
+        [parameter(Position = 0,
+            Mandatory = $true)]
+        [string]$Password
+    ) 
+    $i = 0
+    while ($i -ne 3) {
+        $CharArry = $Password.ToCharArray()
+        $NewPass = $CharArry | Get-Random -Count $CharArry.length
+        $Password = -join $NewPass
+        $i++
+    }
+    return $Password
+}
